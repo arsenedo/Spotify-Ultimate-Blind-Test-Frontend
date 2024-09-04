5b25e1ff8f5d103d393ebf484af46c6e74e1950f
unknown <Student@SIOP0206-EDU-15.EPTM.lan>
2024-09-04 10:27:58 +0200

    Second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {onMount} from "svelte";
+    let socket : WebSocket;
+
+    // Function to send messages
+    function sendMessage() {
+        socket.send(message);
+        message = '';
+    }
+
+    let message = "";
+    let response = "";
+
+    onMount(() => {
+        socket = new WebSocket("ws://localhost:8080");
+        socket.addEventListener("open", () => {
+            console.log("Connection opened");
+        });
+
+        socket.addEventListener("message", (event) => {
+            response = event.data.toString();
+        });
+    })
+</script>
+<div>
+    <h1>Real time chat</h1>
+    <input bind:value={message}/>
+    <button on:click={() => sendMessage()}>Send</button>
+    <div>{response}</div>
+</div>
