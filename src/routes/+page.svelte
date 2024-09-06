0f015c4aff8d66a153463d00badf924746294f79
arsenedo <arsenii.nedorezov@gmail.com>
2024-09-06 11:16:50 +0200

    Created a basic menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,26 @@
     let message = "";
     let response = "";
 
+    const menu =  {
+        hostMenu : false,
+        joinMenu : false,
+    }
+
+    const handleHost = () => {
+        menu.hostMenu = true;
+        menu.joinMenu = false;
+    }
+
+    const handleJoin = () => {
+        menu.hostMenu = false;
+        menu.joinMenu = true;
+    }
+
+    const handleBack = () => {
+        menu.hostMenu = false;
+        menu.joinMenu = false;
+    }
+
     onMount(() => {
         socket = new WebSocket("ws://localhost:8080");
         socket.addEventListener("open", () => {
@@ -27,6 +47,17 @@
     })
 </script>
 <div>
+    {#if menu.hostMenu}
+        <div>Welcome to the host menu</div>
+        <button on:click={() => handleBack()}>Back</button>
+    {:else if menu.joinMenu}
+        <div>Welcome to join menu</div>
+        <button on:click={() => handleBack()}>Back</button>
+    {:else}
+        <h1>Choose</h1>
+        <button on:click={() => handleHost()}>Host</button>
+        <button on:click={() => handleJoin()}>Join</button>
+    {/if}
     <h1>Real time chat</h1>
     <input bind:value={message}/>
     <button on:click={() => registerPlayer()}>Send</button>
