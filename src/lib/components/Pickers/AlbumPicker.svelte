adcdaa2b2b44ae50aac1179d3dd9cd2d465136d6
Arsenii <arsenii.nedorezov@gmail.com>
2024-10-14 21:28:03 +0200

    Update album picker css

--- src/lib/components/Pickers/AlbumPicker.svelte
+++ src/lib/components/Pickers/AlbumPicker.svelte
@@ -42,7 +42,6 @@
 	};
 
 	const handleChooseAlbum = (album) => {
-		console.log(album);
 		const newAlbum = {
 			id: album.id,
 			name: album.name,
@@ -50,10 +49,16 @@
 		};
 		chosenAlbums = [...chosenAlbums, newAlbum];
 		if (chosenAlbums.length >= 2) error = '';
+
+		return false;
 	};
 
 	const handleSearch = () => {
-		if (title.length < 3 || onCooldown) return;
+		if (title.length < 3) {
+			promise = undefined;
+			return
+		}
+		if (onCooldown) return;
 		promise = searchAlbum();
 	};
 
@@ -87,7 +92,7 @@
 						<div>Fetching albums...</div>
 					{:then data}
 						{#each data.albums.items as album}
-							<a on:click={() => handleChooseAlbum(album)} href="0" class="block w-full h-16 [&:not(:last-child)]:border-b-2 border-neutral-200 pb-2">
+							<a on:click={() => handleChooseAlbum(album)} href="#" class="block w-full h-16 [&:not(:last-child)]:border-b-2 [&:not(:last-child)]:border-neutral-200">
 								<div class="w-full h-full flex justify-between items-center gap-2">
 									<img class="h-full" src={album.images[2].url} alt="album_img" />
 									<div>{album.name}</div>
